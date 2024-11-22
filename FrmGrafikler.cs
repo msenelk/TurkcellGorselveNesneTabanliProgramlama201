444b6f6d97293f6e079f47025809b0a96055c67b
msenelk <senel_1996@hotmail.com>
2024-11-22 22:44:56 +0300

    C# üzerinden grafiklere veri çekme işlemi yapıldı.

---
+++ FrmGrafikler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TurkcellGorselveNesneTabanliProgramlama201
+{
+    public partial class FrmGrafikler : Form
+    {
+        public FrmGrafikler()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=MSENELK\SQLEXPRESS;Initial Catalog=DbUrun;Integrated Security=True");
+
+        private void FrmGrafikler_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select Ad, Count(*) From TblUrunler inner join TblKategori \r\non TblUrunler.Kategori=TblKategori.Id\r\nGroup By Ad", baglanti);
+            SqlDataReader dr= komut.ExecuteReader();
+            while (dr.Read())
+            {
+                chart1.Series["Kategori"].Points.AddXY(dr[0],dr[1]);
+            }
+            baglanti.Close();
+        }
+    }
+}
