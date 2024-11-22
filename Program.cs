fcc02bc2e39fb2d76d2c3339da89de444bc3ea90
msenelk <senel_1996@hotmail.com>
2024-11-22 17:13:57 +0300

    İstatistikler için gerekli olan form hazırlandı.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace TurkcellGorselveNesneTabanliProgramlama201
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmUrun());
+            Application.Run(new FrmIstatistik());
         }
     }
 }
