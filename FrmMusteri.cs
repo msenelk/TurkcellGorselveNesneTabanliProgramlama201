bbd37d7f8c6ceb835b0d043a6ddb453885113216
msenelk <senel_1996@hotmail.com>
2024-12-04 19:10:29 +0300

    301 ders programı kapsamında verileri datagridview den araçlara taşıma işlemi gerçekleştirildi.

--- FrmMusteri.cs
+++ FrmMusteri.cs
@@ -36,5 +36,14 @@ namespace TurkcellGorselveNesneTabanliProgramlama201
             MessageBox.Show("Müşteri sistemden silindi");
             dataGridView1.DataSource = tb.MusteriListesi(); // İşem sonrası otomatik güncelleme yapılması için yazdım
         }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMusteriId.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtAd.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtSoyad.Text= dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtSehir.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtBakiye.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+        }
     }
 }
